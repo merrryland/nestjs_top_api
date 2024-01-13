cbb2623197d60d783ffb7a388b72cc45da8b14e1
m.kryukova <m.kryukova@onpoint.ru>
2024-01-13 21:35:22 +0300

    ;fixes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+
 import { AuthModule } from './auth/auth.module';
 import { TopPageModule } from './top-page/top-page.module';
 import { ProductModule } from './product/product.module';
 import { ReviewModule } from './review/review.module';
-import { AuthController } from './auth/auth.controller';
-import { TopPageController } from './top-page/top-page.controller';
 
 @Module({
-  imports: [AuthModule, TopPageModule, ProductModule, ReviewModule],
-  controllers: [AppController, AuthController, TopPageController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    AuthModule,
+    TopPageModule,
+    ProductModule,
+    ReviewModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/top-page/top-page.module.ts
+++ src/top-page/top-page.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { TopPageController } from './top-page.controller';
 
-@Module({})
+@Module({ controllers: [TopPageController] })
 export class TopPageModule {}
